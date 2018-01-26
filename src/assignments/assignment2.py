e46e3f3a00b9d2b855b39799494c1e577a06e00d
i-am-corbs <35587162+i-am-corbs@users.noreply.github.com>
2018-01-25 22:40:20 -0600

    Add files via upload
    class assignment for evaluations

---
+++ src/assignments/assignment2.py
@@ -0,0 +1,57 @@
+def faculty_evaluation_result(nev, rar, som, oft, voft, alw):
+
+    
+    '''
+    Write code to calculate faculty evaluation rating according to asssignment instructions
+
+    :param nev: Never
+    :param rar: Rarely
+    :param som: Sometimes
+    :param oft: Often
+    :param voft: Very Often
+    :param alw: Always
+    :return: rating as a string
+    '''
+   
+
+    total = alw + voft + oft + som + rar + nev
+
+    alw_ratio = alw/total
+    voft_ratio = voft/total
+    oft_ratio = oft/total
+    som_ratio = som/total
+    rar_ratio = rar/total
+    nev_ratio = nev/total
+    
+    if alw_ratio + voft_ratio >= .9:
+        return  "Excellent"
+    elif oft_ratio + voft + alw_ratio >= .8:
+        return  "Very Good"
+    elif som_ratio + oft_ratio + voft_ratio + alw_ratio >= .7:
+        return  "Good"
+    elif rar_ratio + som_ratio + oft_ratio + voft_ratio + alw_ratio >= .6:
+        return "Needs Improvement"
+    elif nev_ratio + rar_ratio + som_ratio + oft_ratio + voft_ratio + alw+ratio <.6:
+        return "Unacceptable"
+
+    
+    
+
+def get_ratings(nev,rar,som, oft,voft, alw):
+    '''
+    Students aren't expected to know this material yet!
+    '''
+    ratings = []
+    total = nev + rar + som + oft + voft + alw
+
+    ratings.append(round(alw / total, 2))
+    ratings.append(round(voft / total, 2))
+    ratings.append(round(oft / total, 2))
+    ratings.append(round(som / total, 2))
+    ratings.append(round(rar / total, 2))
+    ratings.append(round(nev / total, 2))
+
+    return ratings
+    
+
+    
